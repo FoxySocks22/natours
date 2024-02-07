dc2da051e44468aa07d9cc5e452bbbfeaa2c99b3
Foxy Socks <foxysocks1010@gmail.com>
2024-02-07 19:09:11 +0000

    Minor tweaks V1.

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -53,7 +53,7 @@ exports.getCheckOutSession = catchAsync(async(req, res, mext) => {
 const createBookingCheckout = catchAsync(async session => {
     const tour = session.client_reference_id;
     const user = (await User.findOne({ email: session.customer_email })).id;
-    const price = session.line_items[0].amount / 100;
+    const price = session.display_items[0].amount / 100;
     await Booking.create({ tour, user, price });
 
 })
@@ -70,7 +70,7 @@ exports.webhookCheckout = (req, res, next) => {
     } catch(err){
         return res.status(400).send(`Webhook error: ${err.message}`)
     }
-    if(event.type === 'checkout.session.complete') createBookingCheckout(event.data.object);
+    if(event.type === 'checkout.session.completed') createBookingCheckout(event.data.object);
     res.status(200).json({ received: true });
     next();
 } // This is pretty complicated TBH
